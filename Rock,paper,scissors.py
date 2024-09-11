64f14a5e4612e76c1c48760fde8403e1592859ba
Esosa1999 <162116247+Esosa1999@users.noreply.github.com>
2024-09-11 14:40:44 +0100

    Update Rock,paper,scissors.py

--- Rock,paper,scissors.py
+++ Rock,paper,scissors.py
@@ -1,60 +1,58 @@
 import random
 
-User_wins=0
-Computer_wins=0
+User_wins = 0
+Computer_wins = 0
 
-options=['rock', 'paper', 'scissors']
-
+options = ['rock', 'paper', 'scissors']
 
 while True:
-    user_input=input('Type rock/paper/scissors or Q to Quit: ').lower()
-    if user_input.lower()== 'q':
+    user_input = input('Type rock/paper/scissors or Q to Quit: ').lower()
+    if user_input == 'q':
         break
     if user_input not in options:
         continue
-     
-    random_number=random.randint(0,2)
-    #rock=0, paper=1, scissors:2
-    computer_pick=options[random_number]
-    print('computer picked' , computer_pick)
-    
-    if user_input=='rock' and computer_pick=='scissors':
+
+    random_number = random.randint(0, 2)
+    computer_pick = options[random_number]
+    print('Computer picked', computer_pick)
+
+    if user_input == 'rock' and computer_pick == 'scissors':
         print('rock destroys scissors!')
         print('You win!!!')
-        User_wins=User_wins + 1
+        User_wins += 1
         continue
-    if user_input=='rock' and computer_pick=='paper':
-        print ('paper covers rock!!')
-        print('you lose!!') 
-        Computer_wins= Computer_wins + 1
+    if user_input == 'rock' and computer_pick == 'paper':
+        print('paper covers rock!!')
+        print('You lose!!')
+        Computer_wins += 1
         continue
-   
-    if user_input=='paper' and computer_pick=='scissors':
-        print('scissors cuts paper!')
-        print('you lose!!')
-        Computer_wins= Computer_wins +  1
-        continue
-    if user_input=='paper'  and computer_pick=='rock':
-         print ('paper covers rock!!')
-         print ('you win!!')
-         User_wins= User_wins + 1
-         continue
-    
 
-    if user_input=='scissors' and computer_pick=='paper':
+    if user_input == 'paper' and computer_pick == 'scissors':
+        print('scissors cuts paper!')
+        print('You lose!!')
+        Computer_wins += 1
+        continue
+    if user_input == 'paper' and computer_pick == 'rock':
+        print('paper covers rock!!')
+        print('You win!!')
+        User_wins += 1
+        continue
+
+    if user_input == 'scissors' and computer_pick == 'paper':
         print('scissors cuts paper!!')
-        print('you win!!')
-        User_wins= User_wins + 1
+        print('You win!!')
+        User_wins += 1
         continue
-    if user_input=='scissors' and computer_pick=='rock':
+    if user_input == 'scissors' and computer_pick == 'rock':
         print('rock destroys scissors!!')
-        print('you lose!!')
-        Computer_wins= Computer_wins + 1
+        print('You lose!!')
+        Computer_wins += 1
         continue
-    else:
-        if user_input==computer_pick:
-         print('You won' , str(User_wins) , 'times.')
-        print('Computer won', str(Computer_wins) , 'times.')
-        print('The end!')
-        break
-   
+
+    if user_input == computer_pick:
+        print("It's a tie!")
+
+# After the loop ends, print the final results
+print(f'You won {User_wins} times.')
+print(f'Computer won {Computer_wins} times.')
+print('The end!')
